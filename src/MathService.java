f07879625e3b63389a3eb331fac0057c6dbd7098
Prodan Marius <mprodan27@gmail.com>
2017-03-20 22:55:51 +0200

    New

---
+++ src/MathService.java
@@ -0,0 +1,9 @@
+public class MathService {
+    public static boolean isLeapYear(int year) {
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            return true;
+        }
+        return false;
+    }
+
+}
